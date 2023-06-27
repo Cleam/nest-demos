ae386de50fac2392fa4798d5c832c792988b4ede
lizhigao <lizhigao@021.com>
2023-06-27 23:05:29 +0800

    feat: 自定义装饰器

---
+++ custom-decorator/src/app.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, SetMetadata, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Aaa } from './aaa.decorator';
+import { AaaGuard } from './aaa.guard';
+import { Bbb } from './bbb.decorator';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @SetMetadata('aaa', ['admin', 'admin2'])
+  @UseGuards(AaaGuard)
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('h2')
+  @Aaa('admin', 'admin2')
+  @UseGuards(AaaGuard)
+  getHello2(): string {
+    return this.appService.getHello();
+  }
+
+  @Bbb('h3', 'admin', 'admin2')
+  getHello3(): string {
+    return this.appService.getHello();
+  }
+}
